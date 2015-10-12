72821fcb7188ea8e00684ab9ae550bb416e4491b
apolakipso <apolakipso@web.de>
2015-10-12 14:41:23 +0200

    * Simplified folder structure * Added filter get_class * Added test array_of

--- twigextensions/TwigTypeTestTwigExtension.php
+++ twigextensions/TwigTypeTestTwigExtension.php
@@ -15,10 +15,13 @@ class TwigTypeTestTwigExtension extends \Twig_Extension
 	{
 		return array('of_type' => new \Twig_Test_Method($this, 'twig_of_type'));
 	}
-	
+
 	public function getFilters()
 	{
-		return array('get_type' => new \Twig_Filter_Method($this, 'twig_get_type'));
+		return array(
+			'get_type' => new \Twig_Filter_Method($this, 'twig_get_type'),
+			'get_class' => new \Twig_Filter_Method($this, 'twig_get_class')
+		);
 	}
 
 	public function initRuntime(\Twig_Environment $env)
@@ -28,54 +31,67 @@ class TwigTypeTestTwigExtension extends \Twig_Extension
 
 	public function twig_of_type($var, $typeTest=null, $className=null)
 	{
-		
+
 		switch ($typeTest)
 		{
 			default:
 				return false;
 				break;
-		
+
 			case 'array':
 				return is_array($var);
 				break;
-				
+
 			case 'bool':
 				return is_bool($var);
 				break;
-				
+
 			case 'class':
 				return is_object($var) === true && get_class($var) === $className;
 				break;
-		
+
+			case 'array_of':
+				if (!is_array($var) || empty($var)) {
+					return false;
+				}
+				$v = array_shift($var);
+				return is_object($v) && get_class($v) === $className;
+				break;
+
 			case 'float':
 				return is_float($var);
 				break;
-				
+
 			case 'int':
 				return is_int($var);
 				break;
-				
+
 			case 'numeric':
 				return is_numeric($var);
 				break;
-				
+
 			case 'object':
 				return is_object($var);
 				break;
-				
+
 			case 'scalar':
 				return is_scalar($var);
 				break;
-				
+
 			case 'string':
 				return is_string($var);
 				break;
 		}
 	}
-	
+
 	public function twig_get_type($var)
 	{
 		return gettype( $var );
 	}
-	
+
+	public function twig_get_class($var)
+	{
+		return get_class( $var );
+	}
+
 }
